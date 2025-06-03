d2f9c2bae982f5aa0cf1b09654471bac0154927e
AlexObatake-CK <phosphorus@chainkemists.com>
2025-06-03 12:47:55 -0700

    feat: Enable Iris networking and push model

--- Source/IrisErrorExample.Target.cs
+++ Source/IrisErrorExample.Target.cs
@@ -8,6 +8,8 @@ public class IrisErrorExampleTarget : TargetRules
 	public IrisErrorExampleTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		bWithPushModel = true;
+		bUseIris = true;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("IrisErrorExample");
--- Source/IrisErrorExampleEditor.Target.cs
+++ Source/IrisErrorExampleEditor.Target.cs
@@ -8,6 +8,8 @@ public class IrisErrorExampleEditorTarget : TargetRules
 	public IrisErrorExampleEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		bWithPushModel = true;
+		bUseIris = true;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("IrisErrorExample");
